73e821f7e92d9a68c930fd4b6291857ede32ba42
Justin Kim <justinkimstudy@gmail.com>
2018-11-23 18:51:47 -0800

    update
    push

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const title = React.createElement(
-  'h1', // Usually a string that represents an HTML element or tag  TYPE
-  { id: 'main-title', title: 'This is a title'}, // Object containing any attribute or value you want PROPS
-  'My First React Element!' // Contents or Children of the element you're creating  CHILDREN
+// const title = React.createElement(
+//   'h1', // Usually a string that represents an HTML element or tag  TYPE
+//   { id: 'main-title', title: 'This is a title'}, // Object containing any attribute or value you want PROPS
+//   'My First React Element!' // Contents or Children of the element you're creating  CHILDREN
+// );
+
+
+const title = <h1>My First React Element!</h1>;
+
+const desc = <p>I just learned how to create a React node and render it into the DOM</p>;
+
+// const desc = React.createElement('p', null, 'I just learned how to create a React node and render it into the DOM');
+
+const header = React.createElement(
+  'header',
+  null,
+  title,
+  desc
 );
 
-
-const desc = React.createElement('p', null, 'I just learned how to create a React node and render it into the DOM');
-
-const header = React.createElement('header', null, title, desc);
-
-console.log(title);
 
 
 
@@ -17,7 +25,9 @@ console.log(title);
 // React create plain javascript that explains DOM nodes
 // ReactDOM.render()
 
-ReactDOM.render(header,document.getElementById('root')
+ReactDOM.render(
+  header,
+  document.getElementById('root')
 );
 
 // React only manages what gets rendered to the DOM via ReactDOM.render
